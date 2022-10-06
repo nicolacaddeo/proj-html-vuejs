ab2d202019a1413562e9333b78023f2f3eabe5e5
nicolacaddeo <nicola.caddeo58@gmail.com>
2022-10-06 16:45:58 +0200

    created Green Button Component

---
+++ src/components/GreenBtnComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button>
+        {{buttonName}}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'GreenBtnComponent',
+    props: {
+        buttonName: String
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    @import '../style/global_variables.scss';
+
+    button {
+        text-transform: uppercase;
+        background-color: $main-green;
+        border: none;
+        color: $header-links-color;
+        padding: 1rem 2rem;
+        border-radius: 5px;
+        font-size: 1rem;
+        cursor: pointer;
+    }
+</style>
---
+++ src/data/greenBtnData.js
@@ -0,0 +1,9 @@
+export const greenBtnName =
+{
+    getInTouch: "Get in touch",
+    readMore: "Read more",
+    seeAll: "See all",
+    subscribe: 'Subscribe',
+    viewMap: "View map",
+    send: "Send"
+}
