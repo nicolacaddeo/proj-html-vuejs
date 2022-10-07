d22d288e9917790e108610553988d49ff90fd545
nicolacaddeo <nicola.caddeo58@gmail.com>
2022-10-07 16:51:30 +0200

    created transparent button component

---
+++ src/components/subcomponents/TransparentBtnComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button>{{ buttonName }}</button>
+  </div>
+</template>
+
+<script>
+    
+
+export default {
+    name: 'TransparentBtnComponent',
+    props: {
+        buttonName: String
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    @import '../../style/global_variables.scss';
+    button {
+        text-transform: uppercase;
+        background-color: transparent;
+        border: 1px solid $main-green;
+        color: $header-links-color;
+        padding: 1rem 2rem;
+        border-radius: 5px;
+        font-size: 1rem;
+        font-weight: 600;
+        cursor: pointer;
+    }
+
+</style>
